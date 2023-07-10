8b50b21a48c6581ef892f7b603d3c5bc979da4e7
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-07-10 14:27:40 +0800

    refactor: frontend list profile

--- application/views/profile/read.php
+++ application/views/profile/read.php
@@ -1,22 +1,49 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Welcome</title>
-</head>
-<body>
-    <h1>Welcome to My Profile</h1>
 
-    <table>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-        </tr>
-        <?php foreach ($profileData as $profile) { ?>
-            <tr>
-                <td><?php echo $profile['name']; ?></td>
-                <td><?php echo $profile['email']; ?></td>
-            </tr>
-        <?php } ?>
-    </table>
+<head>
+	<title>Profiles</title>
+
+	<!-- Tailwind CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" />
+</head>
+
+<body>
+	<div class="bg-gray-100 p-5 flex flex-col items-center justify-center">
+		<h1 class="text-3xl font-medium text-center mb-5">List Profile</h1>
+
+		<!-- create -->
+		<a href="<?php echo base_url('profile/create'); ?>" class="mb-5 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+			Create New Profile
+			<svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+			</svg>
+		</a>
+
+		<!-- loop -->
+		<div class="grid grid-cols-3 gap-4">
+			<?php foreach ($profileData as $profile) { ?>
+				<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+					<svg class="w-7 h-7 text-gray-500 dark:text-gray-400 mb-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+						<path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+					</svg>
+
+					<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white"><?php echo $profile['name']; ?></h5>
+					<p class="mb-3 font-normal text-gray-500 dark:text-gray-400"><?php echo $profile['email']; ?></p>
+					<a href="<?php echo base_url('profile/view?id=' . $profile['id']); ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+						See Profile
+						<svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+							<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+						</svg>
+					</a>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
+
+	<!-- Script JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 </body>
+
+
 </html>
