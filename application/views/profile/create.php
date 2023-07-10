8634403653798c3b955c073eca8d68f0d10a39dc
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-07-10 18:46:14 +0800

    refactor: frontend

--- application/views/profile/create.php
+++ application/views/profile/create.php
@@ -31,7 +31,7 @@
 					<input type="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required>
 				</div>
 				<div class="mb-6">
-					<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+					<label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
 					<input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="yourmail@mail.com" required>
 				</div>
 				<div class="mb-6">
